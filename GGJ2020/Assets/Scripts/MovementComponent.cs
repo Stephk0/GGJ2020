662dfb943f4d54062f5f4cb7b363141f9dddedd1
RloretWooga <ricardo.rodriguez@wooga.net>
2020-02-01 13:15:19 +0100

    Added FX and movement events to track different states.

---
+++ GGJ2020/Assets/Scripts/MovementComponent.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class MovementComponent : MonoBehaviour
+{
+    public float MovementDistance = 10;
+
+    [SerializeField] AnimationCurve interPolationCurve;
+    [SerializeField] private shipManager _manager;
+    [SerializeField] private float _duration = 0.5f;
+    public float MovementPercent { get; private set; } = 0;
+
+    private Vector3 _initialScale = new Vector3(1, 1, 1);
+
+    private Coroutine _movementCoroutine = null;
+
+    private bool IsMoving { get; set; } = false;
+
+    private void Start()
+    {
+        _manager.OnFinishMovement.AddListener(() =>
+        {
+            IsMoving = false;
+            MovementPercent = 0;
+        });
+        _manager.OnStartMovement.AddListener(() => { IsMoving = true; });
+    }
+
+    private void Update()
+    {
+        Vector3 movementDirection = _manager.GetMovementDirection();
+        if (!movementDirection.sqrMagnitude.Equals(0))
+        {
+            if (_movementCoroutine != null)
+            {
+                StopCoroutine(_movementCoroutine);
+            }
+
+            _movementCoroutine = StartCoroutine(MoveShip(_manager.GetMovementDirection()));
+        }
+    }
+
+    IEnumerator MoveShip(Vector3 direction)
+    {
+        Vector3 initialPos = this.transform.position;
+
+        this.transform.right = direction;
+
+        _manager.OnStartMovement.Invoke();
+        for (float t = 0; t <= _duration; t += Time.deltaTime)
+        {
+            MovementPercent = (t / _duration);
+
+            this.transform.position = Vector3.Lerp(initialPos, initialPos + GetDisplacementVector(direction),
+                interPolationCurve.Evaluate(MovementPercent));
+
+            if (MovementPercent < 0.13f)
+                this.transform.localScale = new Vector3(1f + 30f * interPolationCurve.Evaluate(MovementPercent), 1,
+                    1f - 10f * interPolationCurve.Evaluate(MovementPercent));
+            else
+                this.transform.localScale = _initialScale;
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        MovementPercent = 1;
+        this.transform.position = Vector3.Lerp(initialPos, initialPos + direction * MovementDistance,
+            interPolationCurve.Evaluate(MovementPercent));
+
+        this.transform.localScale = _initialScale;
+        _manager.OnFinishMovement.Invoke();
+    }
+
+    public Vector3 GetDisplacementVector(Vector3 direction)
+    {
+        return (direction * MovementDistance);
+    }
+}
---
+++ GGJ2020/Assets/Scripts/shipManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class shipManager : MonoBehaviour
+{
+    [SerializeField] private InputComponent _input;
+    [SerializeField] private MovementComponent _movement;
+    [SerializeField] private FXComponent _FX;
+
+    [HideInInspector] public UnityEvent OnStartMovement;
+    [HideInInspector] public UnityEvent OnFinishMovement;
+    [HideInInspector] public UnityEvent OnShipMoving;
+
+    public Vector3 GetMovementDirection()
+    {
+        return _input.GetClickDirection().ToXZPlane();
+    }
+
+    public Vector3 GetMovementDisplacementVector()
+    {
+        return _movement.GetDisplacementVector(_input.GetClickDirection().ToXZPlane());
+    }
+
+    public float GetMovementPercent()
+    {
+        return _movement.MovementPercent;
+    }
+}
+
+public static class VectorExtensions
+{
+    public static Vector3 ToXZPlane(this Vector2 position)
+    {
+        return new Vector3(position.x, 0, position.y);
+    }
+}
