e7d8251d47580a5367acc5a3b4e4745997efe906
Stephan Viranyi <stephan.viranyi@wooga.com>
2020-02-01 23:34:24 +0100

    back ground fx

--- GGJ2020/Assets/Scripts/FXSlice.cs
+++ GGJ2020/Assets/Scripts/FXSlice.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,22 +8,41 @@ public class FXSlice : MonoBehaviour
     private Asteroid _asteroid;
 
     private ShipManager ship;
+
+    private bool markForSlicing;
+    private float timeOfSlicing;
     public ParticleSystem sliceEffect;
+
+    public GameObject hideGraphic;
     // Start is called before the first frame update
     void Start()
     {
 
         ship = GameObject.FindWithTag(Tags.SHIP).GetComponent<ShipManager>();
         _asteroid = GetComponent<Asteroid>();
-        _asteroid.OnSlice.AddListener(PlaySliceEffect);
+        _asteroid.OnSlice.AddListener(MarkSlicing);
         
     }
 
+    private void Update()
+    {
+        if (markForSlicing)
+        {
+            PlaySliceEffect();
+            markForSlicing = false;
+        }
+    }
+
+    private void MarkSlicing()
+    {
+        markForSlicing = true;
+    }
     // Update is called once per frame
     void PlaySliceEffect()
     {
         Vector3 dir =ship.transform.position -  transform.position ;
         sliceEffect.transform.rotation = Quaternion.LookRotation(dir, Vector3.up);
         sliceEffect.Play(true);
+        hideGraphic.SetActive(false);
     }
 }
