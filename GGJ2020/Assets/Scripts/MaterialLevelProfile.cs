2c5af8aff903b603931fa5f26a8895749f67efd4
PhilipUnger <pdotunger@gmail.com>
2020-02-01 19:24:10 +0100

    material class updated to materialresource

---
+++ GGJ2020/Assets/Scripts/MaterialLevelProfile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+[CreateAssetMenu(fileName = "Resource_Profile_0", menuName = "Resources/Profile", order = 1)]
+public class MaterialLevelProfile : ScriptableObject
+{
+    public string name;
+    public int level;
+
+    [Space(10)] 
+    
+    public int volume;
+    
+    [FormerlySerializedAs("materialTypes")] public MaterialResource[] materialResourceTypes;
+    public float[] objWeighting;
+}
