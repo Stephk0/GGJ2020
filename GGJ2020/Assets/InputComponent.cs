c52c00ba51f94eb1355e6e18550e17e68892391c
RloretWooga <ricardo.rodriguez@wooga.net>
2020-02-01 01:12:41 +0100

    First movement and input iterations

---
+++ GGJ2020/Assets/InputComponent.cs
@@ -0,0 +1,50 @@
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class InputComponent : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+    private Vector2 _clickDirection;
+
+    private void OnDrawGizmos()
+    {
+        if (_clickDirection.magnitude != 0)
+        {
+            Gizmos.DrawLine(this.transform.position,
+                (transform.position + Vector3.right * _clickDirection.x + Vector3.forward * _clickDirection.y) * 100);
+        }
+    }
+
+    private void Update()
+    {
+        if (!Application.isMobilePlatform)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Vector3 InputPosition = Input.mousePosition;
+                _clickDirection = ComputeClickDirection(InputPosition);
+            }
+            else
+            {
+                _clickDirection = Vector2.zero;
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Nothing implemented for mobile");
+        }
+    }
+
+    private Vector2 ComputeClickDirection(Vector3 pixelCoords)
+    {
+        Vector3 shipViewportPos = _camera.WorldToScreenPoint(this.transform.position);
+        Vector2 direction = (pixelCoords - shipViewportPos);
+
+        return direction.normalized;
+    }
+
+    public Vector2 GetClickDirection()
+    {
+        return this._clickDirection;
+    }
+}
---
+++ GGJ2020/Assets/MovementComponent.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementComponent : MonoBehaviour
+{
+    [SerializeField] private shipManager _manager;
+    [SerializeField] AnimationCurve interPolationCurve;
+    [SerializeField] private float _duration = 0.5f;
+    public float MovementDistance = 10;
+    private bool isMoving = false;
+
+    private Coroutine movementCoroutine = null;
+
+    private void Update()
+    {
+        Vector3 movementDirection = _manager.GetMovementDirection();
+        if (!isMoving && !movementDirection.sqrMagnitude.Equals(0))
+        {
+            movementCoroutine = StartCoroutine(MoveShip(_manager.GetMovementDirection()));
+        }
+    }
+
+    IEnumerator MoveShip(Vector3 direction)
+    {
+        isMoving = true;
+        Vector3 initialPos = this.transform.position;
+        for (float t = 0; t <= _duration; t += Time.deltaTime)
+        {
+            float percent = (t / _duration);
+
+            this.transform.position = Vector3.Lerp(initialPos, initialPos + direction * MovementDistance,
+                interPolationCurve.Evaluate(percent));
+            yield return new WaitForEndOfFrame();
+        }
+
+        this.transform.position = Vector3.Lerp(initialPos, initialPos + direction * MovementDistance,
+            interPolationCurve.Evaluate(1));
+
+        isMoving = false;
+    }
+}
