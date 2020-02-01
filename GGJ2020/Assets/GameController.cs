f5f590f488997d14e735a90c4e3fecc4daf02ee5
nick <n.marouhos@gmail.com>
2020-02-01 13:27:40 +0100

    Add Game scene/controller

---
+++ GGJ2020/Assets/GameController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public int levelNumber = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Start Game");
+        //Determine Level
+        loadLevel(levelNumber);
+        //Initialise UI
+        //Spawn Ship
+
+
+        //Later or never
+        //Generate Obstacles
+        //Generate Dropoff
+    }
+
+    void loadLevel(int num)
+    {
+        print("Load Level " + num);
+
+        //spawnObstcles();
+        //spawnDropoff();
+
+    }
+
+    void spawnObstacles()
+    {
+
+    }
+
+    void spawnDropoff()
+    {
+
+    }
+
+}
