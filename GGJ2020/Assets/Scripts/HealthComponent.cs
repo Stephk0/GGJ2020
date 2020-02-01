93e7e8f1cd70c061e764de12b461dfc410f0b55c
PhilipUnger <pdotunger@gmail.com>
2020-02-01 23:57:39 +0100

    health component hookup

---
+++ GGJ2020/Assets/Scripts/HealthComponent.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthComponent : MonoBehaviour
+{
+    public int health = 100;
+    public int healthDecrease = 4;
+    public float timeToDecrease = 0.5f;
+    public bool decreaseHealth = true;
+    public Action IsDestroyed;
+
+    private float currentTime;
+
+    void Update()
+    {
+        if (decreaseHealth){
+            Timers();
+        }
+    }
+    
+    public void Decrease(int amount)
+    {
+        health -= amount;
+        
+        if(IsDeathReady())
+            DestroyShip();
+    }
+
+    private void Timers()
+    {
+        currentTime += Time.deltaTime;
+
+        if (!(currentTime > timeToDecrease)) 
+            return;
+        
+        currentTime = 0f;
+        Decrease(healthDecrease);
+    }
+
+
+    private bool IsDeathReady()
+    {
+        return health <= 0;
+    }
+
+    private void DestroyShip()
+    {
+        IsDestroyed?.Invoke();
+        Debug.Log("Ship Destroyed");
+    }
+}
